bc10753bc101a05e155cb9699811515dffa57ee8
jorge88dev <jgarcia@gvam.es>
2022-09-21 17:11:32 +0200

    Init vue project.

---
+++ src/views/PostCreationView.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: 'PostCreationView',
+  props: {},
+  data() {
+    return {}
+  },
+  mounted() {
+    console.log('Mounted component')
+  }
+});
+
+</script>
+
+<template>
+
+</template>
